148d2bae6f56ffbcb7b58b10646f4c3ed529c512
Ederson Junior <ed.dev46@gmail.com>
2025-03-28 13:23:27 -0300

    Portfolio Initial Idea

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeroComponent } from "./components/hero/hero.component";
+import { NavbarComponent } from "./components/navbar/navbar.component"
+import { AboutMeComponent } from "./components/about-me/about-me.component"
+import { ProjectsComponent } from "./components/projects/projects.component";
+import { ContactComponent } from "./components/contact/contact.component";
+import { FooterComponent } from "./components/footer/footer.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeroComponent, NavbarComponent, AboutMeComponent, ProjectsComponent, ContactComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
