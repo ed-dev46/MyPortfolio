d6e3e53cde06b397342cf08e4eb5759988e82418
Ederson Junior <ed.dev46@gmail.com>
2025-04-01 01:04:56 -0300

    About Me Section

--- src/app/components/about-me/about-me.component.ts
+++ src/app/components/about-me/about-me.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { TitleDecorationComponent } from "../title-decoration/title-decoration.component";
+import { BracketsTitleComponent } from "../brackets-title/brackets-title.component";
 
 @Component({
   selector: 'app-about-me',
-  imports: [TitleDecorationComponent],
+  imports: [TitleDecorationComponent, BracketsTitleComponent],
   templateUrl: './about-me.component.html',
   styleUrl: './about-me.component.css'
 })
--- src/app/components/brackets-title/brackets-title.component.ts
+++ src/app/components/brackets-title/brackets-title.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-brackets-title',
@@ -8,6 +8,6 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class BracketsTitleComponent {
 
-  @Input() titleText: string = '';
+  @Input() titleText: string = "";
 
 }
