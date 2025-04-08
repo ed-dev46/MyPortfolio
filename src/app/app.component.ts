abd7ded6514e24fa0832dde45971dd4a5935770b
Ederson Junior <ed.dev46@gmail.com>
2025-04-08 13:32:39 -0300

    hero responsivity sm md lg done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,7 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { HeroComponent } from "./components/hero/hero.component";
-import { HeroPageComponent } from "./components/pages/hero-page/hero-page.component"
-import { NavbarComponent } from "./components/navbar/navbar.component"
+import { HeroPageComponent } from "./components/pages/hero-page/hero-page.component";
+import { HeroComponent } from './components/hero/hero.component';
+import { NavbarComponent } from "./components/navbar/navbar.component";
 import { AboutMeComponent } from "./components/about-me/about-me.component"
 import { ProjectsComponent } from "./components/projects/projects.component";
 import { ContactComponent } from "./components/contact/contact.component";
@@ -10,7 +9,7 @@ import { FooterComponent } from "./components/footer/footer.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeroComponent, HeroPageComponent, NavbarComponent, AboutMeComponent, ProjectsComponent, ContactComponent, FooterComponent],
+  imports: [HeroPageComponent, HeroComponent, NavbarComponent, AboutMeComponent, ProjectsComponent, ContactComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
