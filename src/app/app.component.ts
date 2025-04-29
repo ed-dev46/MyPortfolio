c4e242c67ddf0abe254cb536d6eb6bfbae328ad9
Ederson Junior <ed.dev46@gmail.com>
2025-04-29 18:38:03 -0300

    footer added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,10 +4,11 @@ import { NavbarComponent } from "./components/navbar/navbar.component";
 import { MyprojectsPageComponent } from './components/pages/myprojects-page/myprojects-page.component';
 import { AboutmePageComponent } from "./components/pages/aboutme-page/aboutme-page.component";
 import { ContactmePageComponent } from './components/pages/contactme-page/contactme-page.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [HeroPageComponent, NavbarComponent, MyprojectsPageComponent, AboutmePageComponent, ContactmePageComponent, ContactmePageComponent],
+  imports: [HeroPageComponent, NavbarComponent, MyprojectsPageComponent, AboutmePageComponent, ContactmePageComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
